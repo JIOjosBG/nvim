3dbbbf44633f8681865dd35b47b1d43b00d656b4
Yosif Hamed <josif.hameed@gmail.com>
2026-05-17 19:24:50 +0300

    add: toggleable terminal

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -16,3 +16,38 @@ end, { desc = "Toggle spell check" })
 -- fix nearest spelling error and return to position
 -- vim.keymap.set("n", "<leader>sf", "ms[s1z=`s", { desc = "Fix nearest spelling error" })
 
+local float_term_buf = nil
+local float_term_win = nil
+
+vim.keymap.set({ "n", "t" }, "<C-\\>", function()
+	if float_term_win and vim.api.nvim_win_is_valid(float_term_win) then
+		vim.api.nvim_win_hide(float_term_win)
+		float_term_win = nil
+		return
+	end
+
+	local width = math.floor(vim.o.columns * 0.8)
+	local height = math.floor(vim.o.lines * 0.7)
+	local row = math.floor((vim.o.lines - height) / 2)
+	local col = math.floor((vim.o.columns - width) / 2)
+
+	if not (float_term_buf and vim.api.nvim_buf_is_valid(float_term_buf)) then
+		float_term_buf = vim.api.nvim_create_buf(false, true)
+	end
+
+	float_term_win = vim.api.nvim_open_win(float_term_buf, true, {
+		relative = "editor",
+		width = width,
+		height = height,
+		row = row,
+		col = col,
+		style = "minimal",
+		border = "rounded",
+	})
+
+	if vim.bo[float_term_buf].buftype ~= "terminal" then
+		vim.cmd("terminal")
+	end
+
+	vim.cmd("startinsert")
+end)
