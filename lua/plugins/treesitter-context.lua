d77e88405770c13b6935622c2e44f8a49291591e
Yosif Hamed <josif.hameed@gmail.com>
2026-05-19 16:25:08 +0300

    add: sticky lines via treesitter

---
+++ lua/plugins/treesitter-context.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = {
+				"lua", "vim", "vimdoc",
+				"javascript", "typescript", "tsx",
+				"python", "rust", "go",
+				"json", "yaml", "toml",
+				"html", "css",
+				"bash", "markdown",
+			},
+			auto_install = true,
+		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		opts = {
+			enable = true,
+			max_lines = 0,
+			multiline_threshold = 1,
+		},
+	},
+}
